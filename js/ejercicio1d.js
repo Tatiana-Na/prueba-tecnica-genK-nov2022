bde566112665200e17f7e92760874dbc0f309f88
Tatiana-Na <nazarovades@gmail.com>
2022-11-02 16:27:35 +0100

    exercise 1d

--- js/ejercicio1d.js
+++ js/ejercicio1d.js
@@ -4,7 +4,7 @@ const body = document.querySelector('body');
 const containerButtons = document.createElement('div');
 body.append(containerButtons);
 
-for (let i = 1; i<=1000; i++) {
+for (let i = 1; i<=10; i++) {
   const btn = document.createElement('button');
   containerButtons.append(btn); 
   btn.innerText =`${i} botón`
