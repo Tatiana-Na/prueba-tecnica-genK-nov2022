efb9a27d6f8869d81fa46dc8a7bd51f8d240e1e0
Tatiana-Na <nazarovades@gmail.com>
2022-11-03 12:34:04 +0100

    changes 2b

--- js/ejercicio2b.js
+++ js/ejercicio2b.js
@@ -9,3 +9,31 @@ const result = [
     ];
   
   const arrayString= result.toString();
+  let num = 3
+  console.log('56'.padStart(num, '0'));
+  console.log('54'.padStart(num, '0'));
+  console.log('40'.padStart(num, '0'));
+  console.log('128'.padStart(num, '0'));
+  console.log('146'.padStart(num, '0'));
+
+
+  /* const result = [
+  [56, 54, 40],
+  [128, 146, 128],
+  [40, 44, 56],
+  ];
+
+const arrayString= result.toString();
+
+const newArray = Array.from(
+        { length: 1 },
+        (_, i) => ''.padStart(i % 6 + 1, '0') + arrayString + (i)
+    ); 
+
+console.log(newArray);
+*/
+
+
+
+
+  
